4fa72acf637a33e363076eeb66f81c1db84ec3c8
MUKUL <mkltaneja@gmail.com>
2020-08-29 03:06:08 +0530

    Wordbreak method 2 (optimized)

--- Recursion_and_Backtracking/Wordbreak.cpp
+++ Recursion_and_Backtracking/Wordbreak.cpp
@@ -1,8 +1,79 @@
+
+// Method 1 (using hashset)
+
+// #include<iostream>
+// #include<unordered_set>
+// using namespace std;
+
+// void wordbreak(string que, unordered_set<string> &dict, string ans)
+// {
+//     if(que.size() == 0)
+//     {
+//         cout<<ans<<endl;
+//         return;
+//     }
+//     string s = "";
+//     for(int i=0; i<que.size(); i++)
+//     {
+//         s += que[i];
+//         if(dict.find(s) != dict.end())
+//             wordbreak(que.substr(s.size()), dict, ans+s+" ");
+//     }
+// }
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     unordered_set<string> dict;
+//     for(int i=0; i<n; i++)
+//     {
+//         string s;
+//         cin>>s;
+//         dict.insert(s);
+//     }
+//     string sent;
+//     cin>>sent;
+//     wordbreak(sent, dict, "");
+// }
+
+////////////////////////////////////////////////////OR///////////////////////////////////////
+
+// Method 2(using trie)
+// FASTER
 #include<iostream>
-#include<unordered_set>
+#include<vector>
 using namespace std;
 
-void wordbreak(string que, unordered_set<string> &dict, string ans)
+class trie
+{
+    public:
+    vector<trie*> t;
+    bool wordEnd;
+    trie()
+    {
+        t.assign(26,nullptr);
+        wordEnd = false;
+    }
+};
+trie* root = new trie();
+
+void insert(vector<string> &arr)
+{
+    for(string s : arr)
+    {
+        trie* curr = root;
+        for(char c : s)
+        {
+            if(!curr->t[c - 'a'])
+                curr->t[c - 'a'] = new trie();
+            curr = curr->t[c - 'a'];
+        }
+        curr->wordEnd = true;
+    }
+}
+
+void wordbreak(string que, string ans)
 {
     if(que.size() == 0)
     {
@@ -10,11 +81,18 @@ void wordbreak(string que, unordered_set<string> &dict, string ans)
         return;
     }
     string s = "";
+    trie* curr = root;
     for(int i=0; i<que.size(); i++)
     {
         s += que[i];
-        if(dict.find(s) != dict.end())
-            wordbreak(que.substr(s.size()), dict, ans+s+" ");
+        if(curr->t[que[i] - 'a'])
+        {
+            curr = curr->t[que[i] - 'a'];
+            if(curr->wordEnd)
+                wordbreak(que.substr(s.size()), ans+s+" ");
+        }
+        else 
+            break;
     }
 }
 
@@ -22,14 +100,14 @@ int main()
 {
     int n;
     cin>>n;
-    unordered_set<string> dict;
+    vector<string> dict(n);
     for(int i=0; i<n; i++)
     {
-        string s;
-        cin>>s;
-        dict.insert(s);
+        cin>>dict[i];
     }
     string sent;
     cin>>sent;
-    wordbreak(sent, dict, "");
+
+    insert(dict);
+    wordbreak(sent, "");
 }
