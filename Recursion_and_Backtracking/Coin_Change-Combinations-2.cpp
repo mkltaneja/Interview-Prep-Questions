60effaaae751e7640be8c38b7e17c855f5ca964a
MUKUL <mkltaneja@gmail.com>
2020-09-01 16:29:16 +0530

    Coin Change - Combinations - 2

---
+++ Recursion_and_Backtracking/Coin_Change-Combinations-2.cpp
@@ -0,0 +1,61 @@
+
+// APPROACH 1
+// (CALLING COINS WISE)
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void coinchange_combi(int idx, int n, vector<int> &coins, int amt, string ans)
+{
+    if(idx == n)
+    {
+        if(amt == 0)
+            cout<<ans<<"."<<endl;
+        return;
+    }
+    if(amt - coins[idx] >= 0)
+        coinchange_combi(idx, n, coins, amt - coins[idx], ans + to_string(coins[idx]) + "-");
+    coinchange_combi(idx+1, n, coins, amt, ans);
+}
+
+int main()
+{
+    int n, amt;
+    cin>>n;
+    vector<int> coins(n,0);
+    for(int i=0; i<n; i++)
+        cin>>coins[i];
+    cin>>amt;
+    coinchange_combi(0, n, coins, amt, "");
+}
+
+///////////////////////////////////////////OR/////////////////////////////////////////
+
+// APPROACH 2 (CALLING AMOUNT WISE)
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void coinchange_combi(int idx, int n, vector<int> &coins, int amt, string ans)
+{
+    if(amt == 0)
+    {
+        cout<<ans<<"."<<endl;
+        return;
+    }
+    for(int i=idx; i<n; i++)
+        if(amt - coins[i] >= 0)
+            coinchange_combi(i, n, coins, amt - coins[i], ans + to_string(coins[i]) + "-");
+}
+
+int main()
+{
+    int n, amt;
+    cin>>n;
+    vector<int> coins(n,0);
+    for(int i=0; i<n; i++)
+        cin>>coins[i];
+    cin>>amt;
+    coinchange_combi(0, n, coins, amt, "");
+}
