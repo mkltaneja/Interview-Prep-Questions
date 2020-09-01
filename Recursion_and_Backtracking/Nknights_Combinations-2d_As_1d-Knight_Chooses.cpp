5d8f0209c407375cdd401aa2210f3f54770ffb04
MUKUL <mkltaneja@gmail.com>
2020-09-01 15:48:56 +0530

    Nknights Combinations - 2d As 1d - Knight Chooses

--- Recursion_and_Backtracking/Nknights_Combinations-2d_As_1d-Knight_Chooses.cpp
+++ Recursion_and_Backtracking/Nknights_Combinations-2d_As_1d-Knight_Chooses.cpp
@@ -1,3 +1,6 @@
+
+// METHOD 1
+
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -79,4 +82,58 @@ int main()
     vector<vector<char>> board(n,vector<char>(n,'-'));
     vector<vector<int>> notsafe(n,vector<int>(n,0));
     nknights_combi(0, 0, n, notsafe, board);
+}
+
+///////////////////////////////////////////////////OR///////////////////////////////////////
+
+// METHOD 2 (shorter and optimized --> checking previous positions if there is a knight placed)
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool isSafetoPlace(int r, int c, vector<vector<bool>> &board)
+{
+    if(r - 2 >= 0 && c - 1 >= 0 && board[r-2][c-1])
+        return false;
+    if(r - 2 >= 0 && c + 1 >= 0 && board[r-2][c+1])
+        return false;
+    if(r - 1 >= 0 && c - 2 >= 0 && board[r-1][c-2])
+        return false;
+    if(r - 1 >= 0 && c + 2 >= 0 && board[r-1][c+2])
+        return false;
+    return true;
+}
+
+void nknights_combi(int idx, int kpsf, int n, vector<vector<bool>> &board)
+{
+    if(kpsf == n)
+    {
+        for(vector<bool> v : board)
+        {
+            for(bool c : v)
+                c == 1 ? cout<<"k\t" : cout<<"-\t";
+            cout<<endl;
+        }
+        cout<<endl;
+        return;
+    }
+    for(int i=idx; i<n*n; i++)
+    {
+        int r = i / n;
+        int c = i % n;
+        if(!board[r][c] && isSafetoPlace(r,c,board))
+        {
+            board[r][c] = true;
+            nknights_combi(i+1, kpsf+1, n, board);
+            board[r][c] = false;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<vector<bool>> board(n,vector<bool>(n,false));
+    nknights_combi(0, 0, n, board);
 }
