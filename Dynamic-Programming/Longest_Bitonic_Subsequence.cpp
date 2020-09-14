f1ec0cd222acf3a41366f1e6db38541b8bc64851
unknown <mkltaneja@gmail.com>
2020-09-14 13:45:26 +0530

    Maximum Non-Overlapping Bridges

--- Dynamic-Programming/Longest_Bitonic_Subsequence.cpp
+++ Dynamic-Programming/Longest_Bitonic_Subsequence.cpp
@@ -1,27 +1,41 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
-#define f first
-#define s second
 using namespace std;
+
+void LIS(int n, vector<int> &arr, vector<int> &len)
+{
+    for(int i=1; i<n; i++)
+        for(int j=0; j<i; j++)
+            if(arr[j] < arr[i] && len[j]+1 > len[i])
+                len[i] = len[j] + 1;
+}
+
+void LDS(int n, vector<int> &arr, vector<int> &len)
+{
+    for(int i=n-2; i>=0; i--)
+        for(int j=i+1; j<n; j++)
+            if(arr[j] < arr[i] && len[j]+1 > len[i])
+                len[i] = len[j] + 1;
+}
+
 int main()
 {
     int n;
     cin>>n;
-    vector<pair<int,int>> arr(n);
+    vector<int> arr(n,0);
     for(int i=0; i<n; i++)
-        cin>>arr[i].f>>arr[i].s;
-    sort(arr.begin(), arr.end());
-        
-    vector<int> maxnono(n,1);
-    for(int i=1; i<n; i++)
-        for(int j=0; j<i; j++)
-            if(arr[j].f <= arr[i].f && arr[j].s <= arr[i].s && maxnono[j]+1 > maxnono[i])
-                maxnono[i] = maxnono[j] + 1;
-    int maxans = 1;
-    for(int i : maxnono)
-        maxans = max(maxans, i);
-    cout<<maxans;
+        cin>>arr[i];
+    
+    vector<int> maxinc_len(n,1);
+    vector<int> maxdec_len(n,1);
+    LIS(n,arr,maxinc_len);
+    LDS(n,arr,maxdec_len);
+    
+    int LBS_len = 1;
+    for(int i=0; i<n; i++)
+        LBS_len = max(LBS_len, maxinc_len[i]+maxdec_len[i]-1);
+    
+    cout<<LBS_len<<endl;
     
     return 0;
 }
