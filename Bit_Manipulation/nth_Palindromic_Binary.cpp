dd9feaad04d270fb6b40e8c8dd418d5e4a879a09
MUKUL <mkltaneja@gmail.com>
2020-09-16 19:27:48 +0530

    Nth Palindromic Binary

--- Bit_Manipulation/nth_Palindromic_Binary.cpp
+++ Bit_Manipulation/nth_Palindromic_Binary.cpp
@@ -40,3 +40,50 @@ int main()
     cout<<num;
     return 0;
 }
+
+// METHOD 2
+// AC
+#include<iostream>
+using namespace std;
+
+int reversebits(int n, int msbpos)
+{
+    int revans = 0, j = 0;
+    bool f = false;
+    for(int i=31; i>=0; i--)
+    {
+        int mask1 = (1 << i);
+        int mask2 = (1 << j);
+        if(n & mask1)
+        {
+            f = true;
+            revans |= mask2;
+        }
+        if(f)
+            j++;
+    }
+    return revans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int offset = 1, len = 1, size = 1;
+    while(offset + size <= n)
+    {
+        offset += size;
+        size = (1 << ((++len)-1)/2);
+    }
+    int diff = n - offset;
+    int ans = (1 << len-1);
+    ans |= (diff << (len/2));
+    int revans = (ans >> (len/2));
+    revans = reversebits(revans,len);
+    ans |= revans;
+    
+    cout<<ans;
+        
+    return 0;
+}
