e9ee663d58793a0f0b8bc17dd3ec125692d90222
MUKUL <mkltaneja@gmail.com>
2020-10-05 17:25:13 +0530

    Longest Common Substring Method 2

--- Dynamic-Programming/Longest_Common_Substring.cpp
+++ Dynamic-Programming/Longest_Common_Substring.cpp
@@ -1,5 +1,5 @@
 
-// Method 1 --> O(3^n)
+// Method 1 (Void Type) --> O(3^n)
 #include<iostream>
 using namespace std;
 
@@ -23,4 +23,37 @@ int main()
     cin>>s>>t;
     longestcommon_substring(0, 0, 0, s, t);
     cout<<maxlen;
+}
+
+// METHOD 2 (Int type) --> < O(3^n)
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxlen = 0;
+int longest_common_substring(int i, int j, string &s, string &t)
+{
+    if(i == s.size() || j == t.size())
+        return 0;
+    if(s[i] == t[j])
+    {
+        int com = longest_common_substring(i + 1, j + 1, s, t) + 1;
+        maxlen = max(maxlen, com);
+        return com;
+    }
+    
+    longest_common_substring(i + 1, j, s, t);
+    longest_common_substring(i, j + 1, s, t);
+    
+    return 0;
+}
+
+int main()
+{
+    string s, t;
+    cin>>s>>t;
+    longest_common_substring(0, 0, s, t);
+    cout<<maxlen;
+    
+    return 0;
 }
