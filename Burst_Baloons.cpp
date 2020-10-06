a0078798b6370e9212b8ead752e89cc7e7b470a4
MUKUL <mkltaneja@gmail.com>
2020-10-07 02:28:18 +0530

    Burst Baloons TABULATED

--- Burst_Baloons.cpp
+++ Burst_Baloons.cpp
@@ -64,3 +64,42 @@ int main()
     vector<vector<int>> dp(n, vector<int>(n, -1));
     cout << Burst_Baloons(0, n - 1, arr, dp);
 }
+
+// METHOD 3 (TABULATED)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Burst_Baloons(int n, vector<int> &arr, vector<vector<int>> &dp)
+{
+    for (int gap = 0; gap < n; gap++)
+    {
+        for (int si = 0, ei = gap; ei < n; si++, ei++)
+        {
+            int leftcost = (si == 0) ? 1 : arr[si - 1];
+            int rightcost = (ei == n - 1) ? 1 : arr[ei + 1];
+            int maxcoins = 0;
+            for (int cut = si; cut <= ei; cut++)
+            {
+                int leftans = (cut == si) ? 0 : dp[si][cut - 1];
+                int rightans = (cut == ei) ? 0 : dp[cut + 1][ei];
+
+                int myans = leftans + leftcost * arr[cut] * rightcost + rightans;
+                maxcoins = max(maxcoins, myans);
+            }
+            dp[si][ei] = maxcoins;
+        }
+    }
+    return dp[0][n - 1];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    vector<vector<int>> dp(n, vector<int>(n, -1));
+    cout << Burst_Baloons(n, arr, dp);
+}
