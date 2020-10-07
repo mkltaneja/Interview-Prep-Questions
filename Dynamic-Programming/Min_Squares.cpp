5fc7e375ec3d770dfe6e6e983f5e3384c7c5f415
Mukul <mkltaneja@gmail.com>
2020-10-07 22:58:01 +0530

    Min Squares

--- Dynamic-Programming/Min_Squares.cpp
+++ Dynamic-Programming/Min_Squares.cpp
@@ -1,5 +1,5 @@
 
-// METHOD 1 (Recursive)
+// Recursive
 #include<iostream>
 #include<climits>
 using namespace std;
@@ -19,6 +19,34 @@ int main()
     int n;
     cin>>n;
     cout<<minsquares(n);
+    
+    return 0;
+}
+
+// MEMOIZED
+#include<iostream>
+#include<climits>
+#include<vector>
+using namespace std;
+
+int minsquares(int n, vector<int> &dp)
+{
+    if(n == 0 || n == 1 || n == 2 || n == 3)
+        return dp[n] = n;
+    if(dp[n] != -1)
+        return dp[n];
+    int minans = INT_MAX;
+    for(int i = 1; i*i <= n; i++)
+        minans = min(minans, minsquares(n - i*i, dp) + 1);
+    return dp[n] = minans;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> dp(n+1, -1);
+    cout<<minsquares(n, dp);
     
     return 0;
 }
