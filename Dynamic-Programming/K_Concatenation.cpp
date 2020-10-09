c4582bdcfdff532ac40e78b0d17ea6c372873311
Mukul <mkltaneja@gmail.com>
2020-10-10 02:13:22 +0530

    K Concatenation OPTIMIZED

--- Dynamic-Programming/K_Concatenation.cpp
+++ Dynamic-Programming/K_Concatenation.cpp
@@ -29,3 +29,47 @@ int main()
     cin >> k;
     cout << kConcatenation(n, k, arr);
 }
+
+// APPROACH 2 --> O(2*n)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Kedanes(int n, vector<int> &arr)
+{
+    int sum = 0, maxsum = -1e7;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        maxsum = max(sum, maxsum);
+        if (sum < 0)
+            sum = 0;
+    }
+    return maxsum;
+}
+
+int kConcatenation(int n, int k, vector<int> &arr, int arrsum)
+{
+    int ans;
+    if (k == 1)
+        return Kedanes(arr.size() / 2, arr);
+    else
+        ans = Kedanes(arr.size(), arr);
+    ans += (arrsum > 0) ? (k - 2) * arrsum : 0;
+    return ans;
+}
+
+int main()
+{
+    int n, k, sum = 0;
+    cin >> n;
+    vector<int> arr(2 * n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        arr[n + i] = arr[i];
+        sum += arr[i];
+    }
+    cin >> k;
+    cout << kConcatenation(n, k, arr, sum);
+}
