7f0c3cc74067b8952ed897cc88e36d9adeb2e305
Mukul <mkltaneja@gmail.com>
2020-10-20 03:06:33 +0530

    Count Substrings with Unique Characters

--- Hashmap-and-Heaps/Count_Substrings_with_Unique_Characters.cpp
+++ Hashmap-and-Heaps/Count_Substrings_with_Unique_Characters.cpp
@@ -1,3 +1,5 @@
+
+// METHOD 1 (using set)
 #include <iostream>
 #include <unordered_set>
 using namespace std;
@@ -21,6 +23,33 @@ int count_uniquechar_substrings(string &s)
         end++;
     }
     count += ((end - i) * (end - i + 1) / 2) - ((pend - i + 1) * (pend - i + 2) / 2);
+    return count;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << count_uniquechar_substrings(s);
+}
+
+// METHOD 2 (using map)
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int count_uniquechar_substrings(string &s)
+{
+    unordered_map<char, int> freq;
+    int count = 0;
+    int i = 0, end = 0, pend = -1;
+    for (char c : s)
+    {
+        freq[c]++;
+        while (freq[c] == 2)
+            freq[s[i++]]--;
+        count += end++ - i + 1;
+    }
     return count;
 }
 
