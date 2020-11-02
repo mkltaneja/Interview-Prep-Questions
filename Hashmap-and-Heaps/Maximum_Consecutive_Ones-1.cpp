007a66c8bcfabbd897196c16b8fa442315498bfe
Mukul <mkltaneja@gmail.com>
2020-11-03 03:04:23 +0530

    Maximum Cosecutive Ones -1

--- Hashmap-and-Heaps/Maximum_Consecutive_Ones-1.cpp
+++ Hashmap-and-Heaps/Maximum_Consecutive_Ones-1.cpp
@@ -1,3 +1,5 @@
+
+// METHOD 1
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -30,6 +32,42 @@ int maxconsec_ones(int n, vector<int> &arr)
     }
     maxones = max(maxones, preones + 1 + postones);
     return !flag ? maxones - 1 : maxones;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << maxconsec_ones(n, arr);
+
+    return 0;
+}
+
+// METHOD 2
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxconsec_ones(int n, vector<int> &arr)
+{
+    int maxlen = 0;
+    int zeros = 0, j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 0)
+            zeros++;
+        while (zeros == 2)
+        {
+            if (arr[j] == 0)
+                zeros--;
+            j++;
+        }
+        maxlen = max(maxlen, i - j + 1);
+    }
+    return maxlen;
 }
 
 int main()
