d6d69d8c0803968849106b7750e78805cdcb0f89
Mukul <mkltaneja@gmail.com>
2020-12-06 02:38:40 +0530

    Smallest Subarray With All Occurrences Of The Most Frequent Element - CONCISE

--- Hashmap-and-Heaps/Smallest_Subarray_With_All_Occurrences_Of_The_Most_Frequent_Element.cpp
+++ Hashmap-and-Heaps/Smallest_Subarray_With_All_Occurrences_Of_The_Most_Frequent_Element.cpp
@@ -87,3 +87,62 @@ int main()
 
     most_freq_element(n, arr);
 }
+
+// METHOD 2 (CONCISE)
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <climits>
+#define f first
+#define s second
+using namespace std;
+
+void most_freq_element(int n, vector<int> &arr)
+{
+    unordered_map<int, int> fmap;
+    unordered_map<int, int> smap;
+    int maxfreq = 0, minlen = INT_MAX, mf, fei = -1, fsi = -1;
+    for (int i = 0; i < n; i++)
+    {
+        fmap[arr[i]]++;
+        if (smap.find(arr[i]) == smap.end())
+            smap[arr[i]] = i;
+
+        if (fmap[arr[i]] > maxfreq)
+        {
+            maxfreq = fmap[arr[i]];
+            mf = arr[i];
+            fsi = smap[arr[i]];
+            fei = i;
+            minlen = fei - fsi + 1;
+        }
+        else if (fmap[arr[i]] == maxfreq)
+        {
+            int ei = i;
+            int len = ei - smap[arr[i]] + 1;
+            if (len < minlen)
+            {
+                minlen = len;
+                mf = arr[i];
+                fei = i;
+                fsi = smap[arr[i]];
+            }
+        }
+    }
+
+    cout << mf << endl
+         << fsi << endl
+         << fei << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    most_freq_element(n, arr);
+}
